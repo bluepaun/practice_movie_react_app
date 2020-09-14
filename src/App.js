aae795c30950d3ad2c5e6aec2b6040b4aee94180
bluepaun <bluepaun@gmail.com>
2020-09-14 20:37:49 +0900

    add key

--- src/App.js
+++ src/App.js
@@ -5,22 +5,33 @@ function Food({ fav }) {
 }
 
 const foodLikes = [
-    { name: "kimchi" },
-    { name: "pizza" },
-    { name: "hamburger" },
+    {
+        id: 1,
+        name: "kimchi",
+    },
+    {
+        id: 2,
+        name: "pizza",
+    },
+    {
+        id: 3,
+        name: "hamburger",
+    },
 ];
 
 const testJsxList = foodLikes.map((food, index) => {
     return <Food fav={food.name} key={index} />;
 });
 
+const renderFood = (dish, index) => {
+    return <Food fav={dish.name} key={dish.id} />;
+};
+
 function App() {
     return (
         <div className="App">
             <h1>Hello world!</h1>
-            {foodLikes.map((food, index) => {
-                return <Food fav={food.name} key={index} />;
-            })}
+            {foodLikes.map(renderFood)}
             {testJsxList}
             <Food fav="kimchi" something={true} papa={["hello", 1, 2, 3, 4]} />
             <Food fav="ramen" />
